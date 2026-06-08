7cfe0a10bb4d2d3bb6fb4e0f80c2d5b2e6f5a27b
Lucky Yadav <luckyyadav61715@gmail.com>
2026-06-08 23:38:01 +0530

    new changes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,18 +1,14 @@
-import multer  from "multer";
+import multer from "multer"
 
-
- const storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+        cb(null, file.originalname)
     }
-  })
-  
-
-const upload=multer({
-    storage,
-
 })
+
+const upload = multer({ storage })
+
+export { upload }
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,27 +1,22 @@
-class ApiError extends Error{
-
+class ApiError extends Error {
     constructor(
         statusCode,
-        message="something went wrong",
-        error=[],
-        stack=""
-    ){
+        message = "Something went wrong",
+        error = [],
+        stack = ""
+    ) {
         super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.success=false
-        this.errors=this.errors
-
-        if(stack){
-            this.stack=stack
-        }
-        else{
-            Error.captureStackTrace(this,this.constructor)
-
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = error
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
         }
     }
-
 }
 
-export {ApiError}
+export { ApiError }
